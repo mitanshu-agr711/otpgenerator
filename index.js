37ef07493192c88c961df819f6d1452fcf358229
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2025-05-12 10:36:45 +0530

    port change

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 });
 console.log("hebhbhiub");
 
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
